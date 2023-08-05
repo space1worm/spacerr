0e04e93a6fc8f83d2d1bc7969e3a875382d78496
nzurashv <nino.zurashvili@cern.ch>
2023-08-05 14:51:38 +0400

    init

--- components/project.tsx
+++ components/project.tsx
@@ -37,14 +37,14 @@ export default function Project({
             {description}
           </p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
+            {/* {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
                 key={index}
               >
                 {tag}
               </li>
-            ))}
+            ))} */}
           </ul>
         </div>
 
