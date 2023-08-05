17d4be68e5a2612e168153f022475fc80853c4c4
nzurashv <nino.zurashvili@cern.ch>
2023-08-05 16:03:17 +0400

    added eslint rules

--- constants/experiencesData.ts
+++ constants/experiencesData.ts
@@ -1,4 +1,4 @@
-import React from "react";
+import { createElement } from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { LuGraduationCap } from "react-icons/lu";
 
@@ -8,7 +8,7 @@ export const experiencesData = [
     location: "Georgia, Tbilisi",
     description:
       "My journey as a programmer started at CERN, where I learned basics of C++, HTML, CSS, JS, PhP, Node.js and Front-end Development",
-    icon: React.createElement(LuGraduationCap),
+    icon: createElement(LuGraduationCap),
     date: "2018",
   },
   {
@@ -16,7 +16,7 @@ export const experiencesData = [
     location: "Georgia, Tbilisi",
     description:
       "I worked as a Fullstack Developer developer for almost 5 years, where I worked on 12 different projects, I also created core library which is being used in most of the applications. I also upskilled to the full stack and mentored newcomers. My stack included React, Typescript, Three.js, React-Three-Fiber, TailwindCss, Redux-toolkit",
-    icon: React.createElement(CgWorkAlt),
+    icon: createElement(CgWorkAlt),
     date: "2019 - present",
   },
   {
@@ -24,7 +24,7 @@ export const experiencesData = [
     location: "Georgia, Tbilisi",
     description:
       "The reason I decided to start an internship in DevOps was that I had always been interested in how things worked behind the scenes. During my time here, I have learned a lot about systems connections and cloud technology.",
-    icon: React.createElement(LuGraduationCap),
+    icon: createElement(LuGraduationCap),
     date: "2022 - 2022",
   },
   {
@@ -32,7 +32,7 @@ export const experiencesData = [
     location: "Georgia, Tbilisi",
     description:
       "I worked as a fullstack developer at basic-fit around 1 year. My stack included Javascript, jQuery, Scss, Node.js, SFCC",
-    icon: React.createElement(CgWorkAlt),
+    icon: createElement(CgWorkAlt),
     date: "2022 - 2023",
   },
   {
@@ -40,7 +40,7 @@ export const experiencesData = [
     location: "Georgia, Tbilisi",
     description:
       "I worked as a frontend developer at Lookers around 2 month. My stack included Javascript, Typescript, React, Node.js, Storybook, Jest.",
-    icon: React.createElement(CgWorkAlt),
+    icon: createElement(CgWorkAlt),
     date: "2023 - 2023",
   },
   {
@@ -48,15 +48,15 @@ export const experiencesData = [
     location: "Georgia, Tbilisi",
     description:
       "Currently I work at SohoHouse. My stack includes Vue, Nuxt, Typescript, Scss, Jest",
-    icon: React.createElement(CgWorkAlt),
+    icon: createElement(CgWorkAlt),
     date: "2023 - present",
   },
   {
     title: "Mentoring at EPAM and CERN",
     location: "Georgia, Tbilisi",
     description:
-      "Currently, I guide newcomers and train them in the fundamentals of HTML, CSS, JavaScript, TypeScript, and React.",
-    icon: React.createElement(CgWorkAlt),
+      "Currently, I guide newcomers and train them in the fundamentals of HTML, CSS, JavaScript, TypeScript, and React",
+    icon: createElement(CgWorkAlt),
     date: "2023 - present",
   },
 ] as const;
