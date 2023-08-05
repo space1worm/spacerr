00157091f77b1629f86b400e1df0a540d20f7e8e
nzurashv <nino.zurashvili@cern.ch>
2023-08-05 16:06:29 +0400

    fix rules

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// eslint-disable-next-line import/named
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 import { FaPaperPlane } from "react-icons/fa";
 
