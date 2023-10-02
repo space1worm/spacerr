8f367f908c47f6d284c5b6a435e270f251f47cd6
space1worm <kverenchkhiladzeirakli.work@gmail.com>
2023-10-02 14:22:58 +0400

    init

--- components/about.tsx
+++ components/about.tsx
@@ -14,27 +14,31 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] scroll-mt-28 text-center leading-8 sm:mb-40"
+      className="mb-28 max-w-[52rem] scroll-mt-28 text-center leading-8 sm:mb-40"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        I am experienced software developer with 5 years of experience. Skilled in
-        programming, web development, e-commerce, back-end, VR, AR, and video games.
-        Developed core data visualization applications used in{" "}
-        <span className="font-bold text-blue-500">CERN's</span> largest experiment
-        and presented them at ICHEP2020. Currently working as a software developer at
-        EPAM.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing chess,
-        workout, running and reading. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about <span className="font-medium">history and philosophy</span>
+      <p className="mb-3 space-y-3">
+        <p>
+          🛠️ 5 years of hands-on experience in crafting innovative software
+          solutions. Proficient in a wide range of programming languages and
+          technologies for both web and immersive experiences.
+        </p>
+        <p>
+          👨‍💻 At the heart of it, I'm a problem solver. My expertise spans the realms
+          of programming, web development, e-commerce, back-end architecture, and the
+          exciting realms of VR, AR, and video game development.
+        </p>
+        <p>
+          🔬 Notably, I've contributed to pioneering data visualization applications
+          pivotal to <span className="font-bold text-blue-500">CERN's</span>{" "}
+          experiments. My work was showcased at ICHEP2020, highlighting the fusion of
+          technology and science.
+        </p>
+        <p>🚀 Currently working as a Software Developer at SohoHouse</p>
       </p>
     </motion.section>
   );
